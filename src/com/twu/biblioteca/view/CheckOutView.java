cf97408fe1ce2d4362d859168c98245c289d5547
PurnankJain <purnank_jain@yahoo.com>
2015-08-07 16:47:19 +0530

    [Purnank] CheckOutItemView returns next view to render.

--- src/com/twu/biblioteca/view/CheckOutView.java
+++ src/com/twu/biblioteca/view/CheckOutView.java
@@ -1,11 +1,32 @@
 package com.twu.biblioteca.view;
 
 import com.twu.biblioteca.Controller;
+import com.twu.biblioteca.LibrarySection;
 import com.twu.biblioteca.user.User;
 
 public class CheckOutView implements IView {
+
+    LibrarySection librarySection;
+    InputView inputView;
+    String successMessage;
+    String unsuccessfulMessage;
+
+    public CheckOutView(LibrarySection librarySection, InputView inputView, String successMessage, String unsuccessfulMessage) {
+        this.librarySection = librarySection;
+        this.inputView = inputView;
+        this.successMessage = successMessage;
+        this.unsuccessfulMessage = unsuccessfulMessage;
+    }
+
     @Override
     public IView renderView(Controller controller, User user) {
-        return null;
+        String itemName = inputView.readInput();
+        if(librarySection.checkOutItem(itemName)) {
+            System.out.println(successMessage);
+        }
+        else {
+            System.out.println(unsuccessfulMessage);
+        }
+        return controller.nextViewAfterLogin(user);
     }
 }
---
+++ test/com/twu/biblioteca/CheckOutViewTest.java
@@ -0,0 +1,41 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.user.User;
+import com.twu.biblioteca.view.CheckOutView;
+import com.twu.biblioteca.view.InputView;
+import com.twu.biblioteca.view.LibrarianMenuView;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CheckOutViewTest {
+
+    CheckOutView checkOutView;
+    Controller controller;
+    InputView inputView;
+    LibrarySection librarySection;
+    User user;
+    LibrarianMenuView librarianMenuView;
+
+    @Before
+    public void initialise() {
+        librarySection = mock(LibrarySection.class);
+        user = mock(User.class);
+        inputView = mock(InputView.class);
+        controller = mock(Controller.class);
+        librarianMenuView = new LibrarianMenuView(null);
+        checkOutView = new CheckOutView(librarySection, inputView, "", "");
+    }
+
+    @Test
+    public void shouldReturnNextView() {
+        when(inputView.readInput()).thenReturn("");
+        when(librarySection.checkOutItem("")).thenReturn(true);
+        when(controller.nextViewAfterLogin(user)).thenReturn(librarianMenuView);
+
+        assertEquals(LibrarianMenuView.class, checkOutView.renderView(controller, user).getClass());
+    }
+}
